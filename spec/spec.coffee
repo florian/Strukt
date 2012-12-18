c16021ba6ad027909b946b03619b739871c5c67c
Florian H <jscoderr@gmail.com>
2012-12-18 19:51:53 +0100

    add spec for Struct.Explicit

--- spec/spec.coffee
+++ spec/spec.coffee
@@ -33,3 +33,36 @@ describe 'Struct', ->
 
 			john = new Person 'John', 'Doe'
 			expect(msg).to.equal('Welcome John Doe')
+
+describe 'Struct.Explicit', ->
+	it 'should be a function', ->
+		expect(Struct.Explicit).to.be.a('function')
+
+	it 'should return a function', ->
+		expect(new Struct.Explicit).to.be.a('function')
+
+	describe 'instance', ->
+		it 'should return an object', ->
+			expect(new (new Struct.Explicit)).to.be.an('object')
+
+		it 'should set the specified properties', ->
+			Point = new Struct.Explicit 'x', 'y'
+			p = new Point x: 2, y: 40
+			expect(p.x).to.equal(2)
+			expect(p.y).to.equal(40)
+
+		it 'should call the constructor', ->
+			called = false
+			Person = new Struct.Explicit ->
+				called = true
+
+			new Person
+			expect(called).to.be.true
+
+		it 'should be able to access struct properties', ->
+			msg = ''
+			Person = new Struct.Explicit 'forename', 'surname', ->
+				msg = "Welcome #{@forename} #{@surname}"
+
+			john = new Person forename: 'John', surname: 'Doe'
+			expect(msg).to.equal('Welcome John Doe')
--- spec/spec.js
+++ spec/spec.js
@@ -42,4 +42,49 @@
     });
   });
 
+  describe('Struct.Explicit', function() {
+    it('should be a function', function() {
+      return expect(Struct.Explicit).to.be.a('function');
+    });
+    it('should return a function', function() {
+      return expect(new Struct.Explicit).to.be.a('function');
+    });
+    return describe('instance', function() {
+      it('should return an object', function() {
+        return expect(new (new Struct.Explicit)).to.be.an('object');
+      });
+      it('should set the specified properties', function() {
+        var Point, p;
+        Point = new Struct.Explicit('x', 'y');
+        p = new Point({
+          x: 2,
+          y: 40
+        });
+        expect(p.x).to.equal(2);
+        return expect(p.y).to.equal(40);
+      });
+      it('should call the constructor', function() {
+        var Person, called;
+        called = false;
+        Person = new Struct.Explicit(function() {
+          return called = true;
+        });
+        new Person;
+        return expect(called).to.be["true"];
+      });
+      return it('should be able to access struct properties', function() {
+        var Person, john, msg;
+        msg = '';
+        Person = new Struct.Explicit('forename', 'surname', function() {
+          return msg = "Welcome " + this.forename + " " + this.surname;
+        });
+        john = new Person({
+          forename: 'John',
+          surname: 'Doe'
+        });
+        return expect(msg).to.equal('Welcome John Doe');
+      });
+    });
+  });
+
 }).call(this);
